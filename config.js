54c320f7b0a3adfe288dec944d47bb1241e3de0b
yosf22 <yousef.kht2010@gmail.com>
2024-11-11 18:55:21 +0200

    Update config.js
    ..

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-clientId: "", // id bot
-    guildId: "", // id server
-    token: "" //token bot
+clientId: "1269308364773523575", // id bot
+    guildId: "1285372938069213276", // id server
+    token: "MTI2OTMwODM2NDc3MzUyMzU3NQ.GU841U.piuAF-HjMJBvsI26iHr_2id7DeKE7XqwrUvIK8 " //token bot
 }
